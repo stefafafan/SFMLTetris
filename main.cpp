f52e8e50a03a1bca5ee686de482a7803168d3a71
Stefan Alexander <sualexander@wpi.edu>
2014-08-10 16:45:52 +0900

    Added glew

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include <GL/glew.h>
+#include <iostream>
 
 auto checkEvent(sf::RenderWindow &window)
 {
@@ -16,6 +18,14 @@ auto checkEvent(sf::RenderWindow &window)
 auto main() -> int
 {
 	sf::RenderWindow window(sf::VideoMode(800, 600), "OpenGL", sf::Style::Close);
+
+	// Glew test.
+	glewExperimental = GL_TRUE;
+	glewInit();
+	GLuint vertexBuffer;
+	glGenBuffers(1, &vertexBuffer);
+	std::cout << "vertexBuffer: " << vertexBuffer << std::endl;
+
 	while (window.isOpen())
 	{
 		checkEvent(window);
